f680b1e2f9f8b922ec1cf9c9446b70d0d15ccddf
FranSantander <naturatiendacopiapo@gmail.com>
2022-01-10 17:47:18 -0300

    Primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    $("a").click(function () {
+        var gato = this.hash
+
+        $("html, body").animate(
+            {
+                scrollTop: $(gato).offset().top - 70
+            },
+            800
+    }
+})
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
